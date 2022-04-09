80e89a04939c80560135cbf5bd0aad0530f92c2b
Hemanth <hemanth@tibura.com>
2022-04-09 14:28:04 +0530

    changes for push

--- config/plugins/betoni360-parma-ftp-push.js
+++ config/plugins/betoni360-parma-ftp-push.js
@@ -35,71 +35,108 @@
 };
 
 
-// /**
-//  * Response data Mapping
-//  *
-//  * @param {Object} config
-//  * @param {Object} data
-//  * @return {Object}
-//  */
-//  const data = async (config, data) => {
-//      //console.log(data)
-//         return data;
-// };
+
+/**
+ * Detect JSON data.
+ *
+ * @param {String} data
+ * @return {Boolean}
+ */
+ const isJSON = (data) => {
+    try {
+        return !!JSON.parse(data);
+    } catch (e) {
+        return false;
+    }
+};
 
 
-// /**
-//  * Detect JSON data.
-//  *
-//  * @param {String} data
-//  * @return {Boolean}
-//  */
-//  const isJSON = (data) => {
-//     try {
-//         return !!JSON.parse(data);
-//     } catch (e) {
-//         return false;
-//     }
-// };
+/**
+ * Converts CSV to JSON.
+ *
+ * @param {Object} config
+ * @param {Object} response
+ * @return {Object}
+ */
+const response = async (config, response) => {
+    try {
+        if (typeof response.data === 'string' || response.data instanceof String) {
+            response = {
+                data: {
+                    content: response.data,
+                },
+            };
+            // Convert text content to plain data.
+
+            response.data.content = Buffer.from(response.data.content, 'base64').toString('utf-8');
+            response.data.encoding = 'utf-8';
+            if (isJSON(response.data.content)) {
+                response.data.mimetype = 'application/json';
+                response.data.content = JSON.parse(response.data.content);
+            }
+
+        }
+        return response;
+    } catch (e) {
+        console.log(e.message);
+        return response;
+    }
+};
 
 
-// /**
-//  * Converts CSV to JSON.
-//  *
-//  * @param {Object} config
-//  * @param {Object} response
-//  * @return {Object}
-//  */
-//  const response = async (config, response) => {
-//     try {
-//         if (typeof response.data === 'string' || response.data instanceof String) {
-//             response = {
-//                 data: {
-//                     content: response.data,
-//                 },
-//             };
-//             // Convert text content to plain data.
-           
-//                 response.data.content = Buffer.from(response.data.content, 'base64').toString('utf-8');
-//                 response.data.encoding = 'utf-8';
-//                 if (isJSON(response.data.content)) {
-//                     response.data.mimetype = 'application/json';
-//                     response.data.content = JSON.parse(response.data.content);
-//                 }
-            
-//         }
-//         return response;
-//     } catch (e) {
-//         console.log(e.message);
-//         return response;
-//     }
-// };
+/**
+ * Transforms output to Platform of Trust context schema.
+ *
+ * @param {Object} config
+ * @param {Object} output
+ * @return {Object}
+ */
+ const output = async (config, output) => {
+    var arr = [];
+
+    output.data.product.forEach(function (item) {
+
+        item.measurements.forEach((data) => {
+            arr.push({
+                "@type": "Product",
+                "ifcGuid": data.value.content.elementGuid,
+                "design": {
+                    "@type": "Design",
+                    "status": data.value.content.elementDesignStatus
+                },
+                "production": {
+                    "@type": "Production",
+                    "location": data.value.content.fabricationPlant,
+                    "actualEnd": data.value.content.fabricationActualEndDate
+                },
+                "installation": {
+                    "@type": "Installation",
+                    "plannedEnd": data.value.content.erectionPlannedEndDate
+                }
+            })
+
+        });
+    });
+
+    const result = {
+        [config.output.context]: config.output.contextValue,
+        [config.output.object]: {
+            [config.output.array]: arr,
+        },
+    };
+
+    return result;
+}
+
+
 
 
 module.exports = {
     name: 'Betoni360-parma-ftp-push',
     parameters,
-    template
+    template,
+    response,
+    output
     
     
 };
